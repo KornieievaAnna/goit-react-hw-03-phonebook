2b0c71a77892f2600aa5e6adf8898e5a807fe5f0
Anna Kornieieva <109692085+KornieievaAnna@users.noreply.github.com>
2023-03-02 20:57:56 +0200

    add old files

---
+++ src/components/App.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import Form from './Form/Form';
+import ContactList from './ContactList/ContactList';
+import Filter from './Form/Filter/Filter';
+import { Conteiner, Title } from './App.styled';
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  addContact = ({ name, number }) => {
+    const contact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+    this.state.contacts.map(contact => contact.name).includes(name)
+      ? alert(`${name} is already in contact`)
+      : this.setState(prevState => ({
+          contacts: [...prevState.contacts, contact],
+        }));
+  };
+
+  deleteToDo = toDoId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== toDoId),
+    }));
+  };
+
+  filter = () => {
+    return this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(this.state.filter.toLowerCase())
+    );
+  };
+
+  findContact = evt => {
+    // console.log(evt.currentTarget.value);
+    this.setState({ filter: evt.currentTarget.value });
+  };
+
+  render() {
+    return (
+      <Conteiner>
+        <Title>Phonebook</Title>
+        <Form onSubmit={this.addContact} />
+        <Title>Contact</Title>
+        <Filter value={this.state.filter} onChange={this.findContact} />
+        <ContactList contacts={this.filter()} deleteContact={this.deleteToDo} />
+      </Conteiner>
+    );
+  }
+}
+
+export default App;
